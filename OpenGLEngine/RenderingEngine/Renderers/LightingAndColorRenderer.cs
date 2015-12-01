8338166b21688794f46b0e4a72b1e2f28a0a0fb2
Chris <chris127547@hotmail.com>
2015-12-01 15:33:19 -0500

    minor changes

--- OpenGLEngine/RenderingEngine/Renderers/LightingAndColorRenderer.cs
+++ OpenGLEngine/RenderingEngine/Renderers/LightingAndColorRenderer.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace OpenGLEngine.RenderingEngine.Renderers
 {
-    class LightingAndColorRenderer : Renderer
+    public class LightingAndColorRenderer : Renderer
     {
         int shapeData;
         int indiceData;
--- OpenGLEngine/RenderingEngine/Renderers/Renderer.cs
+++ OpenGLEngine/RenderingEngine/Renderers/Renderer.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace OpenGLEngine.RenderingEngine.Renderers
 {
-    interface Renderer
+    public interface Renderer
     {
         void Render();
     }
