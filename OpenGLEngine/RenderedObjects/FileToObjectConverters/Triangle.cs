bd8290818f21d956a4547edf75b32559972fbb6a
Chris <chris127547@hotmail.com>
2016-07-13 18:16:19 -0400

    Changes to triangles to make them easier to use and the ability to get a list of adjacent triangles.

--- OpenGLEngine/RenderedObjects/FileToObjectConverters/Triangle.cs
+++ OpenGLEngine/RenderedObjects/FileToObjectConverters/Triangle.cs
@@ -11,6 +11,20 @@ namespace OpenGLEngine.RenderedObjects.FileToObjectConverters
     {
         public Vertex Vertex1, Vertex2, Vertex3;
         public Vector3 Normal;
+        public int index;
+        private List<Triangle> adjacentTriangles;
+        public List<Triangle> AdjacentTriangles
+        {
+            get
+            {
+                if (adjacentTriangles == null)
+                {
+                    adjacentTriangles = GetAdjacentTriangles();                    
+                }
+                return adjacentTriangles;
+            }
+            set { adjacentTriangles = value; }
+        }
 
         public Triangle(Vertex Vertex1, Vertex Vertex2, Vertex Vertex3, Vector3 Normal)
         {
@@ -20,6 +34,31 @@ namespace OpenGLEngine.RenderedObjects.FileToObjectConverters
         public Triangle(Vertex Vertex1, Vertex Vertex2, Vertex Vertex3)
         {
             this.Vertex1 = Vertex1; this.Vertex2 = Vertex2; this.Vertex3 = Vertex3;
+            Normal = CalculateSurfaceNormal();
+        }
+
+        public Triangle(Vector3 position1, Vector3 position2, Vector3 position3)
+        {
+            Vertex1 = new Vertex();
+            Vertex1.position = new Vertex.Position();
+            Vertex1.position.vector = position1;
+            Vertex2 = new Vertex();
+            Vertex2.position = new Vertex.Position();
+            Vertex2.position.vector = position2;
+            Vertex3 = new Vertex();
+            Vertex3.position = new Vertex.Position();
+            Vertex3.position.vector = position3;
+            ReCalculateNormal();
+            Vertex1.normal = new Vertex.Normal();
+            Vertex1.normal.vector = Normal;
+            Vertex2.normal = new Vertex.Normal();
+            Vertex2.normal.vector = Normal;
+            Vertex3.normal = new Vertex.Normal();
+            Vertex3.normal.vector = Normal;
+        }
+
+        public void ReCalculateNormal()
+        {
             Normal = CalculateSurfaceNormal();
         }
 
@@ -37,6 +76,33 @@ namespace OpenGLEngine.RenderedObjects.FileToObjectConverters
                 normal = Vector3.Cross(temp2 - temp, temp3 - temp);
             }
             return normal.Normalized();
+        }
+
+        private List<Triangle> GetAdjacentTriangles()
+        {
+            List<Triangle> tris = new List<Triangle>();
+            for (int i = 0; i < Vertex1.adjacentTriangles.Count; i++)
+            {
+                if (!tris.Contains(Vertex1.adjacentTriangles[i]))
+                {
+                    tris.Add(Vertex1.adjacentTriangles[i]);
+                }
+            }
+            for (int i = 0; i < Vertex2.adjacentTriangles.Count; i++)
+            {
+                if (!tris.Contains(Vertex2.adjacentTriangles[i]))
+                {
+                    tris.Add(Vertex2.adjacentTriangles[i]);
+                }
+            }
+            for (int i = 0; i < Vertex3.adjacentTriangles.Count; i++)
+            {
+                if (!tris.Contains(Vertex3.adjacentTriangles[i]))
+                {
+                    tris.Add(Vertex3.adjacentTriangles[i]);
+                }
+            }
+            return tris;
         }
     }
 }
